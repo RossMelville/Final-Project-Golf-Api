ef2e29e4f1b1eb3d2196358a9af57586223a1b73
Ross Melville <rossmelville@hotmail.com>
2017-10-05 12:09:41 +0100

    Added routes to get shot by id and create new shot

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -1,6 +1,22 @@
 class ShotsController < ApplicationController
-    def index  
-      @shots = [ {start_point: 56.345838, end_point: 56.348190} ]
-            render :json => @shots
-    end
+
+  def show_params
+    params.require(:shot).permit([:start_lat, :start_lon, :end_lat, :end_lon])
   end
+
+  def index  
+    @shots = [ {start_lat: 56.345838, start_lon: -2.808940, end_lat: 56.348190, end_lon: -2.810899} ]
+    render :json => @shots
+  end
+
+  def shot
+    shot = Shot.find(params[:id])
+    render :json => shot
+  end
+
+  def create
+    shot = Shot.create(shot_params)
+    render :json => shot
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'shots' => 'shots#index'
+  scope path: "api" do
+    resources :shots
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
